2def6a2fb8905269a2b0d79f51312e9ecea24bba
jinojacob01 <jinojacob456@gmail.com>
2022-11-01 09:50:08 +0530

    Nov1

---
+++ SeleniumFrameworkDesign/src/main/java/AutomationTraining/App.java
@@ -0,0 +1,13 @@
+package AutomationTraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
